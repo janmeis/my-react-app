b554cbb2a3718f26cdd16dbf89c32f86ee874954
Jan Meissner <janmeis@yahoo.co.uk>
2025-01-21 13:56:02 +0100

    division to several useEffect

--- src/components/ApiRequestComponent.tsx
+++ src/components/ApiRequestComponent.tsx
@@ -7,62 +7,88 @@ import { IFolder } from '../model/folder';
 import { InputText } from 'primereact/inputtext';
 import './ApiRequestComponent.scss';
 
+interface AudioSource {
+  dirId: { [key: string]: string };
+  artist?: string;
+  source: 'artist' | 'album' | 'track';
+}
+
+const yearAlbumRegex = /^(?:\[([^\[]*)\]\s*)?(.+)$/;
+
 const ApiRequestComponent: React.FC = () => {
-  const yearAlbumRegex = /^(?:\[([^\[]*)\]\s*)?(.+)$/;
   const [folders, setFolders] = useState<IFolder[]>([]);
   const [sid, setSid] = useState<string>('');
-  const [dirIds, setDirIds] = useState<string[]>([]);
-  const [artist, setArtist] = useState<string>('');
+  const [audioSource, setAudioSource] = useState<AudioSource>({
+    dirId: {},
+    source: 'artist',
+  });
+
   const tableRef = React.createRef<DataTable<IFolder[]>>();
 
-  const titleHeaderTemplate = () => {
-    const title = dirIds.length === 0
-      ? 'Artist'
-      : (dirIds.length === 1
-        ? ''
-        : 'Track');
-    return <span>{ title}</span>;
-  };
-
-  const titleBodyTemplate = (rowData: IFolder) => {
-    const onTitleClick = (): void => {
-      setFolders([]);
-      setDirIds((prevDirIds) => [...prevDirIds, rowData.id]);
-      setArtist(() => dirIds.length === 0 ? rowData.title : '');
-      if (dirIds.length === 0)
-        tableRef.current?.saveState();
-    }
-
-    return rowData.track
-      ? <span>{rowData.title}</span>
-      : <a id="tableLink" onClick={() => onTitleClick()}>{rowData.title}</a>;
-  };
-
-  const yearBodyTemplate = (rowData: IFolder) => {
-    const year = dirIds.length === 1
-      ? parseAlbum(rowData.title).year
-      : rowData.year;
-    return <span>{year}</span>;
-  };
-
-  const albumBodyTemplate = (rowData: IFolder) => {
-    const album = dirIds.length === 1
-      ? parseAlbum(rowData.title).album
-      : rowData.album;
-    return <span>{album}</span>;
-  };
-
   const artistBodyTemplate = (rowData: IFolder) => {
-    return <span>{artist || rowData.artist}</span>;
+    const onArtistClick = (): void => {
+      setFolders([]);
+      setAudioSource(prevState => {
+        const _audioSource = {
+          artist: rowData.title,
+          source: 'album',
+        } as AudioSource;
+        prevState.dirId['artist'] = rowData.id;
+        return { ..._audioSource, dirId: prevState.dirId };
+      });
+      tableRef.current?.saveState();
+    };
+
+    return (
+      <a id='tableLink' onClick={() => onArtistClick()}>
+        {rowData.title}
+      </a>
+    );
   };
 
-  const parseAlbum = (album: string | undefined): { album: string, year: string; } => {
+  const albumArtistBodyTemplate = () => <span>{audioSource.artist}</span>;
+
+  const parseAlbum = (
+    album: string | undefined
+  ): { album: string; year: string } => {
     const match = yearAlbumRegex.exec(album || '');
     return {
       album: match ? match[2] : '',
-      year: match ? match[1] : ''
-    }; 
-  }
+      year: match ? match[1] : '',
+    };
+  };
+
+  const albumBodyTemplate = (rowData: IFolder) => {
+    const onAlbumClick = (): void => {
+      setAudioSource(prevState => {
+        const _audioSource = {
+          artist: prevState.artist,
+          source: 'track',
+        } as AudioSource;
+        prevState.dirId['album'] = rowData.id;
+        return { ..._audioSource, dirId: prevState.dirId };
+      });
+    };
+    const album = parseAlbum(rowData.title).album;
+    return (
+      <a id='tableLink' onClick={() => onAlbumClick()}>
+        {album}
+      </a>
+    );
+  };
+
+  const yearBodyTemplate = (rowData: IFolder) => {
+    const year = parseAlbum(rowData.title).year;
+    return <span>{year}</span>;
+  };
+
+  const getFolders = async (dirId?: string): Promise<IFolder[]> => {
+    let url = `${config.baseUrl}/folder`;
+    if (dirId) url += `?dirId=${dirId}`;
+    const response = await axios.get(url);
+    let _folders = response.data as IFolder[];
+    return _folders;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -78,17 +104,11 @@ const ApiRequestComponent: React.FC = () => {
   }, []);
 
   useEffect(() => {
+    if (audioSource.source !== 'artist') return;
+
     const fetchData = async () => {
       try {
-        let url = `${config.baseUrl}/folder`;
-        if (dirIds.length > 0) {
-          const dirId = dirIds[dirIds.length - 1];
-          url += `?dirId=${dirId}`;
-        }
-        const response = await axios.get(url);
-        let _folders = response.data as IFolder[];
-        if (_folders.length > 0 && _folders[0].track) 
-          _folders =_folders.sort((a, b) => (a.album || '').localeCompare(b.album || '') || (a.disc || 0) - (b.disc || 0) || (a.track || 0) - (b.track || 0));
+        let _folders = await getFolders();
         setFolders(_folders);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -96,35 +116,139 @@ const ApiRequestComponent: React.FC = () => {
     };
 
     fetchData();
-  }, [sid, dirIds]);
+  }, [sid, audioSource]);
+
+  useEffect(() => {
+    if (audioSource.source !== 'artist') return;
+    tableRef.current?.restoreState();
+  }, [audioSource]);
+
+  useEffect(() => {
+    if (audioSource.source !== 'album') return;
+
+    const fetchData = async () => {
+      try {
+        let _folders = await getFolders(audioSource.dirId['artist']);
+        setFolders(_folders);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [audioSource]);
+
+  useEffect(() => {
+    if (audioSource.source !== 'track') return;
+
+    const fetchData = async () => {
+      try {
+        let _folders = await getFolders(audioSource.dirId['album']);
+        _folders = _folders.sort(
+          (a, b) =>
+            (a.album || '').localeCompare(b.album || '') ||
+            (a.disc || 0) - (b.disc || 0) ||
+            (a.track || 0) - (b.track || 0)
+        );
+        setFolders(_folders);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [audioSource]);
 
   const onBackClick = (): void => {
     setFolders([]);
-    setDirIds((prevDirIds) => prevDirIds.slice(0, prevDirIds.length - 1));
-    setArtist(() => dirIds.length === 2 && folders.length > 0 ? folders[0].artist || '' : '');
-    if (dirIds.length === 1)
-      tableRef.current?.restoreState();
+    if (audioSource.source === 'track') {
+      setAudioSource(prevState => ({
+        ...prevState,
+        source: 'album',
+      }));
+    } else if (audioSource.source === 'album') {
+      setAudioSource(() => ({
+        dirId: {},
+        source: 'artist',
+      }));
+    }
   };
 
   return (
     <div>
-      <div id="sidDiv">
-        <label htmlFor="sid">sid</label>
-        <InputText type="text" className="p-inputtext" id="sid" name="sid" value={sid} disabled />
+      <div id='sidDiv'>
+        <label htmlFor='sid'>sid</label>
+        <InputText
+          type='text'
+          className='p-inputtext'
+          id='sid'
+          name='sid'
+          value={sid}
+          disabled
+        />
       </div>
 
-      <h3>{dirIds.length > 0 && (<a id="back" onClick={() => onBackClick()}><i className="pi pi-arrow-left" /></a>)}</h3>
-      <DataTable value={folders} stripedRows scrollable scrollHeight="75vh" paginator rows={25} rowsPerPageOptions={[10, 25, 50, 100]} tableStyle={{ minWidth: '50rem' }} ref={tableRef}>
-        <Column field="id" header="ID" style={{width: '5%'}}/>
-        <Column header={titleHeaderTemplate} body={titleBodyTemplate} />
-        <Column header="Year" body={yearBodyTemplate} style={{ width: '10%' }} />
-        <Column header="Album" body={albumBodyTemplate} />
-        <Column header="Artist" body={artistBodyTemplate} />
-        <Column field="durationString" header="Duration" />
-        <Column field="filesizeString" header="File size" />
-        <Column field="disc" header="Disc" />
-        <Column field="track" header="Track" />
-      </DataTable>
+      <h3>
+        {audioSource.source !== 'artist' && (
+          <a id='back' onClick={() => onBackClick()}>
+            <i className='pi pi-arrow-left' />
+          </a>
+        )}
+      </h3>
+      {audioSource.source === 'artist' && (
+        <DataTable
+          value={folders}
+          stripedRows
+          scrollable
+          scrollHeight='75vh'
+          paginator
+          rows={25}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          tableStyle={{ minWidth: '50rem' }}
+          ref={tableRef}
+        >
+          <Column field='id' header='ID' style={{ width: '5%' }} />
+          <Column header='Artist' body={artistBodyTemplate} />
+        </DataTable>
+      )}
+      {audioSource.source === 'album' && (
+        <DataTable
+          value={folders}
+          stripedRows
+          scrollable
+          scrollHeight='75vh'
+          paginator
+          rows={25}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          tableStyle={{ minWidth: '50rem' }}
+        >
+          <Column field='id' header='ID' style={{ width: '5%' }} />
+          <Column header='Artist' body={albumArtistBodyTemplate} />
+          <Column header='Album' body={albumBodyTemplate} />
+          <Column header='Year' body={yearBodyTemplate} />
+        </DataTable>
+      )}
+      {audioSource.source === 'track' && (
+        <DataTable
+          value={folders}
+          stripedRows
+          scrollable
+          scrollHeight='75vh'
+          paginator
+          rows={25}
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          tableStyle={{ minWidth: '50rem' }}
+        >
+          <Column field='id' header='ID' style={{ width: '5%' }} />
+          <Column field='artist' header='Artist' />
+          <Column field='album' header='Album' />
+          <Column field='year' header='Year' />
+          <Column field='disc' header='Disc' />
+          <Column field='track' header='Track' />
+          <Column field='durationString' header='Duration' />
+          <Column field='filesizeString' header='File size' />
+        </DataTable>
+      )}
     </div>
   );
 };
