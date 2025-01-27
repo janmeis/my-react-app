adbc85e46a110d1a2ad7978b3f1669ac8145fa7e
Jan Meissner <janmeis@yahoo.co.uk>
2025-01-27 19:18:05 +0100

    breadcrumb

--- src/components/ApiRequestComponent.tsx
+++ src/components/ApiRequestComponent.tsx
@@ -21,6 +21,8 @@ interface AudioSource {
 const yearAlbumRegex = /^(?:\[([^\[]*)\]\s*)?(.+)$/;
 
 const ApiRequestComponent: React.FC = () => {
+  const baseUrl = `${location.protocol}//${location.hostname}:${config.port}`;
+  const [isSidDisplayed, setIsSidDisplayed] = useState<boolean>(true);
   const [folders, setFolders] = useState<IFolder[]>([]);
   const [sid, setSid] = useState<string>('');
   const [audioSource, setAudioSource] = useState<AudioSource>({
@@ -29,10 +31,12 @@ const ApiRequestComponent: React.FC = () => {
     source: 'artist',
   } as AudioSource);
   const [checked, setChecked] = useState<boolean>(false);
-  const [selectedLetter, setSelectedLetter] = useState<string>('a');
+  const [selectedLetter, setSelectedLetter] = useState<string>('1');
   const [cover, setCover] = useState<string>('');
 
   const tableRef = React.createRef<DataTable<IFolder[]>>();
+
+  setTimeout(() => setIsSidDisplayed(false), 5000);
 
   const onArtistClick = (rowData: IFolder): void => {
     setFolders([]);
@@ -42,8 +46,8 @@ const ApiRequestComponent: React.FC = () => {
 
   const iconBodyTemplate = (rowData: IFolder, onClick: (rowData: IFolder) => void) => (
     <a className='artist-icon' onClick={_ => onClick(rowData)}>
-      <FontAwesomeIcon id='fa-folder' icon={faFolder} size='lg' />
-      <FontAwesomeIcon id='fa-folder-open' icon={faFolderOpen} size='lg' />
+      <FontAwesomeIcon id='fa-folder' icon={faFolder} size='xl' />
+      <FontAwesomeIcon id='fa-folder-open' icon={faFolderOpen} size='xl' />
     </a>
   );
 
@@ -83,7 +87,7 @@ const ApiRequestComponent: React.FC = () => {
   };
 
   const getFolders = async (dirId?: string): Promise<{ total: number; cover: string; folders: IFolder[] }> => {
-    let url = `${config.baseUrl}/folder`;
+    let url = `${baseUrl}/folder`;
     if (dirId) url += `?dirId=${dirId}`;
     const response = await axios.get(url);
     let _folders = response.data.folders as IFolder[];
@@ -93,7 +97,7 @@ const ApiRequestComponent: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`${config.baseUrl}/auth`);
+        const response = await axios.get(`${baseUrl}/auth`);
         setSid(response.data.data.sid);
       } catch (error) {
         setSid(JSON.stringify(error));
@@ -170,22 +174,57 @@ const ApiRequestComponent: React.FC = () => {
     }
   };
 
-  const getLetterRegex = (letter: string): RegExp => new RegExp(`^(?:${letter}|${letter.toUpperCase()})`);
+  const testFirstLetter = (title: string, selectedLetter: string): boolean => {
+    const firstLetterRegex = (letter: string): RegExp => new RegExp(`^(?:${letter.toUpperCase()})`);
+
+    if (selectedLetter === '1') return /[0-9]/.test(title.charAt(0));
+
+    let firstLetter = title.replace("'", '').charAt(0).toLocaleUpperCase();
+    firstLetter = firstLetter.normalize('NFD').replace(/[\u0300-\u036f]/g, ''); // remove accents - <see="https://www.codu.co/articles/remove-accents-from-a-javascript-string-skgp1inb"/>
+    firstLetter = firstLetter.replace('Ø', 'O'); // fix some special characters
+
+    return /[a-z]/.test(selectedLetter)
+      ? firstLetterRegex(selectedLetter).test(firstLetter)
+      : !/[a-z0-9]/i.test(firstLetter);
+  };
 
   return (
     <div>
-      <div id='sidDiv'>
+      <div id='sidDiv' style={{ display: isSidDisplayed ? 'block' : 'none' }}>
         <label htmlFor='sid'>sid</label>
         <InputText type='text' className='p-inputtext' id='sid' name='sid' value={sid} disabled />
       </div>
 
-      <h3>
+      <div className='mb-5'>
         {audioSource.source !== 'artist' && (
-          <a id='back' onClick={_ => onBackClick()}>
-            <i className='pi pi-arrow-left' />
-          </a>
+          <div id='breadcrumb' className='flex justify-content-left mb-3'>
+            {audioSource.source === 'album' && (
+              <>
+                <a
+                  id='artist-lnk'
+                  className='pi pi-angle-right mr-3'
+                  style={{ fontSize: '2.5rem' }}
+                  onClick={_ => onBackClick()}
+                />
+                <div className='text-4xl'>{audioSource.artist.title}</div>
+              </>
+            )}
+            {audioSource.source !== 'album' && (
+              <>
+                <div id='artist-lnk' className='pi pi-angle-right mr-3' style={{ fontSize: '2.5rem' }} />
+                <div className='text-4xl'>{audioSource.artist.title}</div>
+                <a
+                  id='album-lnk'
+                  className='pi pi-angle-right ml-3 mr-3'
+                  style={{ fontSize: '2.5rem' }}
+                  onClick={_ => onBackClick()}
+                />
+                <div className='text-4xl'>{parseAlbum(audioSource.album.title).album}</div>
+              </>
+            )}
+          </div>
         )}
-      </h3>
+      </div>
       {audioSource.source === 'artist' && (
         <>
           <div className='flex justify-content-left mb-3'>
@@ -202,22 +241,20 @@ const ApiRequestComponent: React.FC = () => {
           {checked && (
             <>
               <div className='flex column-gap-2 mb-5'>
-                {[...Array(26).keys()]
-                  .map(i => String.fromCharCode(i + 97))
-                  .map(letter => (
-                    <ToggleButton
-                      key={letter}
-                      className='w-3rem'
-                      onLabel={letter}
-                      offLabel={letter}
-                      checked={selectedLetter === letter}
-                      onChange={_ => setSelectedLetter(letter)}
-                    />
-                  ))}
+                {['1', ...[...Array(26).keys()].map(i => String.fromCharCode(i + 97)), '高'].map(letter => (
+                  <ToggleButton
+                    key={letter}
+                    className='w-3rem'
+                    onLabel={letter}
+                    offLabel={letter}
+                    checked={selectedLetter === letter}
+                    onChange={_ => setSelectedLetter(letter)}
+                  />
+                ))}
               </div>
               <div className='flex flex-wrap column-gap-1 row-gap-2'>
                 {folders
-                  .filter(folder => getLetterRegex(selectedLetter).test(folder.title))
+                  .filter(folder => testFirstLetter(folder.title, selectedLetter))
                   .map((folder, index) => (
                     <Tag
                       id='artist-tag'
@@ -231,46 +268,53 @@ const ApiRequestComponent: React.FC = () => {
             </>
           )}
           {!checked && (
-            <DataTable
-              value={folders}
-              stripedRows
-              scrollable
-              scrollHeight='75vh'
-              paginator
-              rows={25}
-              rowsPerPageOptions={[10, 25, 50, 100]}
-              tableStyle={{ minWidth: '50rem' }}
-              ref={tableRef}
-            >
-              <Column body={rowData => iconBodyTemplate(rowData, onArtistClick)} style={{ width: '0.35rem' }} />
-              <Column field='id' header='ID' style={{ width: '5%' }} />
-              <Column header='Artist' body={rowData => artistBodyTemplate(rowData)} />
-            </DataTable>
+            <div style={{ maxWidth: '65vw' }}>
+              <DataTable
+                value={folders}
+                stripedRows
+                scrollable
+                scrollHeight='75vh'
+                paginator
+                rows={25}
+                rowsPerPageOptions={[10, 25, 50, 100]}
+                ref={tableRef}
+              >
+                <Column body={rowData => iconBodyTemplate(rowData, onArtistClick)} style={{ width: '0.35rem' }} />
+                <Column field='id' header='ID' style={{ width: '5%' }} />
+                <Column header='Artist' body={rowData => artistBodyTemplate(rowData)} />
+              </DataTable>
+            </div>
           )}
         </>
       )}
       {audioSource.source === 'album' && (
-        <DataTable
-          value={folders}
-          stripedRows
-          scrollable
-          scrollHeight='75vh'
-          paginator
-          rows={25}
-          rowsPerPageOptions={[10, 25, 50, 100]}
-          tableStyle={{ minWidth: '50rem' }}
-        >
-          <Column body={rowData => iconBodyTemplate(rowData, onAlbumClick)} style={{ width: '0.35rem' }} />
-          <Column field='id' header='ID' style={{ width: '5%' }} />
-          <Column header='Artist' body={albumArtistBodyTemplate} />
-          <Column header='Album' body={albumBodyTemplate} />
-          <Column header='Year' body={yearBodyTemplate} />
-        </DataTable>
+        <div style={{ maxWidth: '65vw' }}>
+          <DataTable
+            value={folders}
+            stripedRows
+            scrollable
+            scrollHeight='75vh'
+            paginator
+            rows={25}
+            rowsPerPageOptions={[10, 25, 50, 100]}
+            tableStyle={{ maxWidth: '65vw' }}
+          >
+            <Column body={rowData => iconBodyTemplate(rowData, onAlbumClick)} style={{ width: '0.35rem' }} />
+            <Column field='id' header='ID' style={{ width: '5%' }} />
+            <Column header='Artist' body={albumArtistBodyTemplate} />
+            <Column header='Album' body={albumBodyTemplate} />
+            <Column header='Year' body={yearBodyTemplate} />
+          </DataTable>
+        </div>
       )}
       {audioSource.source === 'track' && (
         <div className='grid'>
           <div className='col-2'>
-            <Image src={cover} alt='cover' width='350' height='350' />
+            <div className='flex flex-column mt-5'>
+              <Image src={cover} alt='cover' width='250' height='250' />
+              <div className='text-2xl font-italic mt-3'>{audioSource.artist.title}</div>
+              <div className='text-2xl'>{parseAlbum(audioSource.album.title).album}</div>
+            </div>
           </div>
           <div className='col'>
             <DataTable
@@ -281,12 +325,11 @@ const ApiRequestComponent: React.FC = () => {
               paginator
               rows={25}
               rowsPerPageOptions={[10, 25, 50, 100]}
-              tableStyle={{ minWidth: '50rem' }}
             >
               <Column
                 body={
                   <span className='artist-icon'>
-                    <FontAwesomeIcon id='fa-music' icon={faMusic} size='lg' />
+                    <FontAwesomeIcon id='fa-music' icon={faMusic} size='xl' />
                   </span>
                 }
                 style={{ width: '0.5rem' }}
